afa94c8819c5b694cc970dec9e65530f7521d1b5
Arjay1412 <timbancayaarjay@gmail.com>
2024-11-22 07:14:21 +0800

    dynamic pie chart: commented pie chart

--- fireApp-boilerplate/projectsite/projectsite/urls.py
+++ fireApp-boilerplate/projectsite/projectsite/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
-    path('stations', views.map_station, name='map-station'),
     path('chart/', PieCountbySeverity, name='chart'),
+    path('stations', views.map_station, name='map-station'),
 ]
