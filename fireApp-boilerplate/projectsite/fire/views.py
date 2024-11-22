bb182984c36185ef1a8787c810227f31e52970b9
Arjay1412 <timbancayaarjay@gmail.com>
2024-11-22 14:54:44 +0800

    dynamic multibar chart

--- fireApp-boilerplate/projectsite/fire/views.py
+++ fireApp-boilerplate/projectsite/fire/views.py
@@ -24,7 +24,7 @@ class ChartView(ListView):
         pass
 
 def PieCountbySeverity(request): 
-    query = ''' SELECT severity_level, COUNT(*) as count FROM fire_incident GROUP BY severity_level;''' 
+    query = '''SELECT severity_level, COUNT(*) as count FROM fire_incident GROUP BY severity_level;''' 
     data = {}
     with connection.cursor() as cursor: 
         cursor.execute(query)
@@ -60,22 +60,19 @@ def LineCountbyMonth(request):
     return JsonResponse(result_with_month_names)
 
 def MultilineIncidentTop3Country(request):
-    query = '''
-    SELECT fl.country,strftime('%m', fi.date_time) AS month,COUNT(fi.id) AS incident_count
-    FROM fire_incident fi
-    JOIN fire_locations fl ON fi.location_id = fl.id
-    WHERE fl.country IN (
-    SELECT fl_top.country
-    FROM fire_incident fi_top
-    JOIN fire_locations fl_top ON fi_top.location_id = fl_top.id
-    WHERE strftime('%Y', fi_top.date_time) = strftime('%Y', 'now')
-    GROUP BY fl_top.country
-    ORDER BY COUNT(fi_top.id) DESC
-    LIMIT 3 )
-    AND strftime('%Y', fi.date_time) = strftime('%Y', 'now')
-    GROUP BY fl.country, month
-    ORDER BY fl.country, month;
-    '''
+    query ='''
+    SELECT fl.country,strftime('%m', fi.date_time) AS month,COUNT(fi.id) AS incident_count 
+    FROM fire_incident fi 
+    JOIN fire_locations fl ON fi.location_id = fl.id WHERE fl.country 
+    IN (SELECT fl_top.country FROM fire_incident fi_top 
+        JOIN fire_locations fl_top ON fi_top.location_id = fl_top.id 
+        WHERE strftime('%Y', fi_top.date_time) = strftime('%Y', 'now') 
+        GROUP BY fl_top.country 
+        ORDER BY COUNT(fi_top.id) DESC LIMIT 3 ) 
+        AND strftime('%Y', fi.date_time) = strftime('%Y', 'now') 
+        GROUP BY fl.country, month 
+        ORDER BY fl.country, month; '''
+    
     with connection.cursor() as cursor:
         cursor.execute(query)
         rows = cursor.fetchall()
@@ -91,13 +88,12 @@ def MultilineIncidentTop3Country(request):
         country = row[0]
         month = row[1]
         total_incidents = row[2]
-
-    # If the country is not in the result dictionary, initialize it with all months set to zero
-    if country not in result:
-        result[country] = {month: 0 for month in months}
-
-    # Update the incident count for the corresponding month
-    result[country][month] = total_incidents
+        # If the country is not in the result dictionary initialize it with all months set to zero
+        if country not in result:
+            result[country] = {month: 0 for month in months}
+            
+            # Update the incident count for the corresponding month
+        result[country][month] = total_incidents
 
     # Ensure there are always 3 countries in the result
     while len(result) < 3:
@@ -108,6 +104,36 @@ def MultilineIncidentTop3Country(request):
     for country in result:
         result[country] = dict(sorted(result[country].items()))
         
+    return JsonResponse(result)
+
+def multipleBarbySeverity(request):
+    query = '''
+    SELECT fi.severity_level, strftime('%m', fi.date_time) AS month, COUNT(fi.id) AS incident_count
+    FROM fire_incident fi
+    GROUP BY fi.severity_level, month
+    '''
+    
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+    
+    result = {}
+    months = set(str(i).zfill(2) for i in range(1, 13))
+    
+    for row in rows:
+        level = str(row[0]) # Ensure the severity level is a string
+        month = row[1]
+        total_incidents = row[2]
+        
+        if level not in result:
+            result[level] = {month: 0 for month in months}
+            
+        result[level][month] = total_incidents
+        
+    # Sort months within each severity level
+    for level in result:
+        result[level] = dict(sorted(result[level].items()))
+    
     return JsonResponse(result)
 
 def map_station(request):
--- fireApp-boilerplate/projectsite/projectsite/urls.py
+++ fireApp-boilerplate/projectsite/projectsite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView,ChartView,PieCountbySeverity,LineCountbyMonth, MultilineIncidentTop3Country
+from fire.views import HomePageView,ChartView,PieCountbySeverity,LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity
 from fire import views
 
 urlpatterns = [
@@ -10,6 +10,7 @@ urlpatterns = [
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
     path('chart/', PieCountbySeverity, name='chart'),
     path('lineChart/', LineCountbyMonth, name='chart'),
-    path('multilinechart/', MultilineIncidentTop3Country, name='chart'),
+    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
     path('stations', views.map_station, name='map-station'),
+    path('multiBarChart/', multipleBarbySeverity, name='chart'),
 ]
